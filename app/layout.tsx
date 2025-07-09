6d076a234967082b77bede1a349b03b997309f33
John Noriega <johnnoriega22@icloud.com>
2025-07-08 21:52:30 -0500

    adding the setup to the next application using tailwind shadcn.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Theme } from "@radix-ui/themes"
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -27,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Theme accentColor="tomato">
+          {children}
+        </Theme>
       </body>
     </html>
   );
