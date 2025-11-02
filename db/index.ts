9fe253ce70b2c6ad2da046f6fd3e5520907ff098
J-commits22 <joshua.hernandeznorieg01@utrgv.edu>
2025-11-02 15:48:53 -0600

    connected Neon DB with prod and dev configs

--- db/index.ts
+++ db/index.ts
@@ -1,13 +1,23 @@
 import { drizzle as drizzleNeon } from 'drizzle-orm/neon-http'
 import { drizzle as drizzlePostgres } from 'drizzle-orm/node-postgres'
 import { neon } from '@neondatabase/serverless'
-
+import { Pool } from 'pg'
 import * as schema from './schema'
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL environment variable is not set')
+}
 
 export const db = process.env.VERCEL
   ? drizzleNeon({
-    client: neon(process.env.DATABASE_URL!),
-    schema,
-    casing: 'snake_case',
-  })
-  : drizzlePostgres(process.env.DATABASE_URL!, { schema, casing: 'snake_case' })
+      client: neon(process.env.DATABASE_URL!),
+      schema,
+      casing: 'snake_case',
+    })
+  : drizzlePostgres({
+      client: new Pool({
+        connectionString: process.env.DATABASE_URL,
+      }),
+      schema,
+      casing: 'snake_case',
+    })
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,10 @@
 import type { Config } from 'drizzle-kit'
 import 'dotenv/config'
+import * as dotenv from 'dotenv';
+
+
+const envFile = process.env.NODE_ENV === 'production' ? '.env.prod' : '.env.dev';
+dotenv.config({ path: envFile });
 
 export default {
   dialect: 'postgresql',
